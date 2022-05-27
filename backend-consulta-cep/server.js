3759f9a673acd2e4a9a673e1c596e55c631f2e83
pedro21medeiros <pedro21medeiros@gmail.com>
2022-05-27 17:18:18 -0300

    Feature: Implementação da interação front com back, trazendo endereço da API

---
+++ backend-consulta-cep/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  const { cep } = req.query;
+  fetch(`https://ws.apicep.com/cep.json?code=${cep}`)
+    .then(response => response.json())
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.send(err);
+      console.log(err);
+    })
+});
