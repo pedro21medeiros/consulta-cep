0898115d2ab5fbf8d0ba10dde19fe5ad5a3332e8
pedro21medeiros <pedro21medeiros@gmail.com>
2022-05-30 17:10:40 -0300

    Fix: Modifica a pasta api para melhorar o deploy

--- api/communication.js
+++ api/communication.js
@@ -1,8 +1,26 @@
-export default function handler(req, res) {
+const express = require('express');
+const cors = require('cors');
+const { default: fetch } = require('node-fetch');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
   const { cep } = req.query;
   fetch(`https://ws.apicep.com/cep.json?code=${cep}`)
-  .then(res => res.json())
-  .then(data => {
-    res.send(data);
-  })
-}
+    .then(res => res.json())
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.send(err);
+      console.log(err);
+    });
+});
+
+module.exports = app;
