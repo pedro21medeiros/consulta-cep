f284f5fe9d8d9fd20055b46eba4074c28b3462b4
pedro21medeiros <pedro21medeiros@gmail.com>
2022-05-30 16:45:31 -0300

    Fix: Ajusta arquivo adicional para deploy Vercel e ajusta server.js

--- api/communication.js
+++ api/communication.js
@@ -1,24 +1,8 @@
-const express = require('express');
-const fetch = require('node-fetch');
-const cors = require('cors');
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(cors());
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
-
-app.get('/', (req, res) => {
+export default function handler(req, res) {
   const { cep } = req.query;
   fetch(`https://ws.apicep.com/cep.json?code=${cep}`)
-    .then(response => response.json())
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.send(err);
-      console.log(err);
-    });
-});
+  .then(res => res.json())
+  .then(data => {
+    res.send(data);
+  })
+}
